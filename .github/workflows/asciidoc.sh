94351cdfe933a6260664e4fed2a782780ae49ab5
eroccowaruico <naohito@analog.bz>
2020-04-17 13:55:17 +0900

    スタイルのパスを追加

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -22,7 +22,7 @@ ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a imagesdir=${CURRENT_PATH}/images  -r asciidoctor-diagram   index.adoc
 
 
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk  -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram -r ${CURRENT_PATH}/configs/config.rb   -a allow-uri-read index.adoc
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk  -a pdf-styledir=${CURRENT_PATH}/themes  -a pdf-style=${CURRENT_PATH}/themes/sample-theme.yml　-a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram -r ${CURRENT_PATH}/configs/config.rb   -a allow-uri-read index.adoc
 
 
 asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/ebub/ -o sample.epub  -r asciidoctor-diagram -a imagesdir=${CURRENT_PATH}/images  index.adoc
