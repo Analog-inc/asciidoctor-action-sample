9278dd1c032985502855f40515c099151843c9b7
eroccowaruico <naohito@analog.bz>
2020-04-21 16:42:35 +0900

    Themaを利用せず日本語の出力を試す

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -21,6 +21,6 @@ CURRENT_PATH=`pwd`
 
 asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a imagesdir=${CURRENT_PATH}/images  -r asciidoctor-diagram   index.adoc
 
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk    -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram -r ${CURRENT_PATH}/configs/config.rb   -a allow-uri-read index.adoc
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk         -a allow-uri-read index.adoc
 
 asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/ebub/ -o sample.epub  -r asciidoctor-diagram -a imagesdir=${CURRENT_PATH}/images  index.adoc
