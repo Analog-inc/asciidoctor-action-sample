a7b9fa17050448e7d457c82cb19592b71f12ea81
eroccowaruico <naohito@analog.bz>
2020-04-21 16:55:21 +0900

    テーマファイルの場所を絶対パスで指定

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -7,6 +7,7 @@ mkdir -p ./outputs/ebub
 
 
 CURRENT_PATH=`pwd`
+ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 
 
 # asciidoctor command arguments
@@ -21,6 +22,6 @@ CURRENT_PATH=`pwd`
 
 asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a imagesdir=${CURRENT_PATH}/images  -r asciidoctor-diagram   index.adoc
 
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk         -a allow-uri-read index.adoc
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk    -a pdf-styledir=${ASCIIDOCTOR_PDF_DIR}/data/themes    -a allow-uri-read index.adoc
 
 asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/ebub/ -o sample.epub  -r asciidoctor-diagram -a imagesdir=${CURRENT_PATH}/images  index.adoc
