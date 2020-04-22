175138cb1fe114de33759499e499b6a3c00912b7
eroccowaruico <naohito@analog.bz>
2020-04-22 10:35:14 +0900

    pdf生成からダイアグラムが抜けていたので追加

--- configs/config.rb
+++ configs/config.rb
@@ -1,8 +1,3 @@
 Prawn::Svg::Font::GENERIC_CSS_FONT_MAPPING.merge!(
-  'sans-serif' => 'GenShinGothic'
-  'Sans Serif' => 'GenShinGothic'
-  'M+ 1mn' => 'GenShinGothic'
-  'Noto Serif' => 'GenYoMin'
-  'Serif' => 'GenYoMin'
-
+  'sans-serif' => 'Sans Serif'
 )
