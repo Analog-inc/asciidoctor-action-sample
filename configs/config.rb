d34f82163e9ba0d9dd3a4d31d4c54dafaeaa5599
eroccowaruico <naohito@analog.bz>
2020-04-22 10:24:54 +0900

    フォントの構成を変更

--- configs/config.rb
+++ configs/config.rb
@@ -1,3 +1,8 @@
 Prawn::Svg::Font::GENERIC_CSS_FONT_MAPPING.merge!(
   'sans-serif' => 'GenShinGothic'
+  'Sans Serif' => 'GenShinGothic'
+  'M+ 1mn' => 'GenShinGothic'
+  'Noto Serif' => 'GenYoMin'
+  'Serif' => 'GenYoMin'
+
 )
