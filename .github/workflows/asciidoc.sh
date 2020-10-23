46e2f09e3f893f25c266f74fdf7428cad904ca9e
eroccowaruico <naohito@analog.bz>
2020-10-23 10:19:00 +0900

    認証を追加

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -23,8 +23,24 @@ echo ${GITHUB_REF}
 # -d, --doctype = DOCTYPE
 # -r, --require = LIBRARY
 
+if [[ ${GITHUB_EVENT_NAME} =~ .*release.* ]]; then
+  is_release=true
+else
+  is_release=false
+fi
+
+DATA_URI=""
+
+if "${is_release}"; then
+  cp -f -r  ${CURRENT_PATH}/images ./outputs/html
+else
+  mkdir -p ./outputs/html/images
+  cp -f -r  ${CURRENT_PATH}/images/video ./outputs/html/images
+  DATA_URI="-a data-uri"
+fi
+
 COMMON_PARAMETERS=" -B ${CURRENT_PATH}/  -a target-release  -r asciidoctor-diagram -v "
-HTML_PARAMETERS=" -D ${CURRENT_PATH}/outputs/html/ -a docinfodir=${CURRENT_PATH}/docinfo -a toc-title=目次 "
+HTML_PARAMETERS=" -D ${CURRENT_PATH}/outputs/html/ -a docinfodir=${CURRENT_PATH}/docinfo -a toc-title=目次 ${DATA_URI}"
 PDF_PARAMETERS="  -D ${CURRENT_PATH}/outputs/pdf/  -a chapter-label=  -r ${CURRENT_PATH}/configs/config.rb -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -a scripts=cjk -a allow-uri-read "
 
 set -x
@@ -33,8 +49,6 @@ set -x
 # Output HTML
 asciidoctor ${COMMON_PARAMETERS}  ${HTML_PARAMETERS} -o "index.html"  -a target-sample  index.adoc
 asciidoctor ${COMMON_PARAMETERS}  ${HTML_PARAMETERS} -o "README.html" README.adoc
-mkdir -p ./outputs/html/images
-cp -f -r  ${CURRENT_PATH}/images/video ./outputs/html/images
 # Output PDF
 asciidoctor-pdf ${COMMON_PARAMETERS}  ${PDF_PARAMETERS} -o "サンプル.pdf"   -a target-sample   -a pdf-style=${CURRENT_PATH}/themes/user-sample-theme.yml   index.adoc
 asciidoctor-pdf ${COMMON_PARAMETERS}  ${PDF_PARAMETERS} -o "README.pdf"   -a pdf-style=${CURRENT_PATH}/themes/common-theme.yml README.adoc
