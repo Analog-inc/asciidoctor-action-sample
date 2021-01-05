651a5250e1b533c4f0e232e10f06a9030a325606
eroccowaruico <naohito@analog.bz>
2021-01-05 21:40:27 +0900

    自動化対応

--- images/SvgBadges/maketool/make.sh
+++ images/SvgBadges/maketool/make.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+set -eu
 
-ALL=`cat ./all.svg`
-CHECK=`cat ./check.svg`
-EYE=`cat ./eye.svg`
-PEN=`cat ./pen.svg`
-USER=`cat ./user.svg`
-SMARTPHONE=`cat ./smartphone.svg`
-FEATUREPHONE=`cat ./featurephone.svg`
-
+TOOL_DIR=$(cd $(dirname $0); pwd)
+ALL=`cat ${TOOL_DIR}/all.svg`
+CHECK=`cat ${TOOL_DIR}/check.svg`
+EYE=`cat ${TOOL_DIR}/eye.svg`
+PEN=`cat ${TOOL_DIR}/pen.svg`
+USER=`cat ${TOOL_DIR}/user.svg`
+SMARTPHONE=`cat ${TOOL_DIR}/smartphone.svg`
+FEATUREPHONE=`cat ${TOOL_DIR}/featurephone.svg`
+BADGES_DIR=${TOOL_DIR/SvgBadges\/maketool/SvgBadges\/badges}
 
 while IFS=, read data value
 do
     eval $data=$value
-done <  ./size.csv
-    
-    
+done <  ${TOOL_DIR}/size.csv
+
+
 while IFS=, read id code name num color
 do
-   
+
     TEMP=$ALL
     NUMLEN=`echo " scale=5; ${num} * ${STEPLEN} " | bc`
     FULLLENNUM=`echo " scale=5; ${NUMLEN} + ${FULLLEN} " | bc`
@@ -25,7 +27,7 @@ do
     BLENNUM=`echo " scale=5; ${NUMLEN} + ${BLEN} " | bc`
     CLENNUM=`echo " scale=5; ${NUMLEN} + ${CLEN} " | bc`
     DLENNUM=`echo " scale=5; ${NUMLEN} + ${DLEN} " | bc`
-    
+
     TEMP=${TEMP//\{%cord%\}/${code}}
     TEMP=${TEMP//\{%name%\}/${name}}
     TEMP=${TEMP//\{%color%\}/${color}}
@@ -35,11 +37,11 @@ do
     TEMP=${TEMP//\{%c-length%\}/${CLENNUM}}
     TEMP=${TEMP//\{%d-length%\}/${DLENNUM}}
 
-    echo ${TEMP} | sed -e "s|{%icon%}|$CHECK|g" > "badge-user-${id}-check.svg"
-    echo ${TEMP} | sed -e "s|{%icon%}|$EYE|g" > "badge-user-${id}-eye.svg"
-    echo ${TEMP} | sed -e "s|{%icon%}|$PEN|g" > "badge-user-${id}-pen.svg"
-    echo ${TEMP} | sed -e "s|{%icon%}|$USER|g" > "badge-user-${id}-user.svg"
-    echo ${TEMP} | sed -e "s|{%icon%}|$SMARTPHONE|g" > "badge-user-${id}-smartphone.svg"
-    echo ${TEMP} | sed -e "s|{%icon%}|$FEATUREPHONE|g" > "badge-user-${id}-featurephone.svg"
-    
-done < ./data.csv
+    echo ${TEMP} | sed -e "s|{%icon%}|$CHECK|g" > "${BADGES_DIR}/badge-user-${id}-check.svg"
+    echo ${TEMP} | sed -e "s|{%icon%}|$EYE|g" > "${BADGES_DIR}/badge-user-${id}-eye.svg"
+    echo ${TEMP} | sed -e "s|{%icon%}|$PEN|g" > "${BADGES_DIR}/badge-user-${id}-pen.svg"
+    echo ${TEMP} | sed -e "s|{%icon%}|$USER|g" > "${BADGES_DIR}/badge-user-${id}-user.svg"
+    echo ${TEMP} | sed -e "s|{%icon%}|$SMARTPHONE|g" > "${BADGES_DIR}/badge-user-${id}-smartphone.svg"
+    echo ${TEMP} | sed -e "s|{%icon%}|$FEATUREPHONE|g" > "${BADGES_DIR}/badge-user-${id}-featurephone.svg"
+
+done < ${TOOL_DIR}/data.csv
