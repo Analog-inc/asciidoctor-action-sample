d77ba269af4dc5829f76ef15f80b8c5847ed298a
eroccowaruico <naohito@analog.bz>
2021-03-21 02:18:20 +0900

    初期openを可能に

--- js/docinfo.js
+++ js/docinfo.js
@@ -3,13 +3,17 @@ function insert_accordion_buttons() {
   let i = 0;
   for (let accordion_block of accordions) {
     i++;
+    openCheck = "";
     let classnames = '';
     for (let blockClass of accordion_block.classList) {
-    	if(blockClass!='accordion'){
+    	if(blockClass != 'accordion'){
     		classnames = classnames + ' ' + blockClass;
     	}
+      if(blockClass == 'accordion-open'){
+    		openCheck = "checked";
+    	}
     }
-    accordion_block.insertAdjacentHTML('beforebegin','<input type="checkbox" class="accordion-check" id="accordion-check-' + i + '" /><label class="accordion-label ' + classnames +'" for="accordion-check-' + i + '"></label>');
+    accordion_block.insertAdjacentHTML('beforebegin','<input type="checkbox" class="accordion-check" id="accordion-check-' + i + '" ' + openCheck + ' /><label class="accordion-label ' + classnames +'" for="accordion-check-' + i + '"></label>');
   }
 }
 window.onload = function(){
