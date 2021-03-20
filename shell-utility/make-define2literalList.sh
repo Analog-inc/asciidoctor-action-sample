1d47fbe9ed407eed94f8e7efb09c367d6e46a89f
eroccowaruico <naohito@analog.bz>
2021-03-21 02:21:01 +0900

    便利シェルを追加
    定義ファイルの内容をリスト化
    利用していないファイルを削除

---
+++ shell-utility/make-define2literalList.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -u
+
+TOOL_DIR=$(cd $(dirname $0); pwd)
+INDEX_DIR=${TOOL_DIR%%\/shell-utility}
+FILE_ARRAY=()
+
+##############################
+# 以下にに定義した値を 「literal-list.adoc」として出力します
+##############################
+FILE_ARRAY+=("${INDEX_DIR}/define-literal.adoc")
+FILE_ARRAY+=("${INDEX_DIR}/define-document.adoc")
+
+##############################
+# 以下に出力します
+##############################
+CSV_FILE="${INDEX_DIR}/literal-list.adoc"
+
+echo "" > "${CSV_FILE}"
+
+for FILE in "${FILE_ARRAY[@]}"
+do
+
+  while read data value1 value2 value3
+  do
+    if  [[ ${data} =~ :name-.*:  ]] ||  [[ ${data} =~ :flow-.*:  ]] ||  [[ ${data} =~ :link-.*:  ]]
+    then
+      echo "| "${data//:/}" | "${value1}" "${value2}" "${value3} | sed 's/+/+\'$'\n/g' >> ${CSV_FILE}
+    fi
+    if  [[ ${data} =~ //.* ]] && [[ ! ${data} =~ ////.* ]]
+    then
+      echo "2+h| "${data//\/\//}" "${value1}" "${value2}" "${value3} >> ${CSV_FILE}
+    fi
+  done < ${FILE}
+
+done
